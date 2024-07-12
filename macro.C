3d2aa5458cfdb59ed387f6aaee152f18319cc19a
Robot 4 Jarvis <2010jarvis@gmail.com>
2024-07-12 21:48:09 +0200

    Reading and fitting graphs

--- macro.C
+++ macro.C
@@ -29,9 +29,18 @@ double GEL(double *x, double *par) {  // gauss + erc + lin
     // The function is:
     // fval = gauss(x, p0,p1) * p2 + p3*x + p4 * Erfx (x-p0)*p5 + p6
 }
+double GEL2(double *x, double *par) { // double gaussian + background. The parameters of the extra gaussian are 7,8 and 9 (10 in total)
+    double fval; double xval; xval = *x;
+    fval = TMath::Gaus(xval, par[0], par[1],false)*par[2]  + TMath::Gaus(xval, par[7],par[8],false) * par[9] + par[3]*(xval-par[0]) + par[4] * TMath::Erfc((xval-par[0])*par[5]) + par[6];
 
-// This function, given a histname (that must be inside a Histograms.root file), a xmin and a xmax (ROI), fits a gaussian function (+background) and returns the fitted function
-TF1 *fitGEL(TH1D *hist, double xmin, double xmax){
+    return fval;
+}
+double pol2(double *x, double *par) {  // function y = a*x*x + b*x + c
+    double fval; double xval; xval = *x;
+    fval = xval *xval* par[0] + xval* par[1] + par[2];
+    return fval;
+}
+TF1 *fitGEL(TH1D *hist, double xmin, double xmax, char erc = '1'){
 
     // we open the file with the Histograms and retreive the second one
     //TFile *f = new TFile("Histograms.root");
@@ -49,16 +58,38 @@ TF1 *fitGEL(TH1D *hist, double xmin, double xmax){
         funGEL->SetParLimits(1,0,(xmax-xmin));
         funGEL->SetParLimits(2,0,1.5*ymax);
         funGEL->SetParLimits(4,0,1.5*ymax);
-        funGEL->FixParameter(4,0);   funGEL->FixParameter(5,0); 
-
-
+        if(erc == '0') {funGEL->FixParameter(4,0);};
     hist->Fit("funGEL","QR+"); //The R parameter restricts the fitting to [xmin, xmax]
 
     return funGEL;
 }
+TF1 *fitGEL2(TH1D *hist, double xmin, double xmid, double xmax, char erc = '1'){
+
+    // we open the file with the Histograms and retreive the second one
+    //TFile *f = new TFile("Histograms.root");
+    //TH1D *hist = (TH1D*)f->Get(histName);
+
+    // we draw it (just to see that it worked)
+    hist->GetXaxis()->SetRangeUser(xmin, xmax);
+    double ymax = hist->GetBinContent(hist->GetMaximumBin());
+    // We try fitting a gaussian with the following parameters
+    TF1 *funGEL2 = new TF1("funGEL2", GEL2, xmin, xmax, 10); 
+        // We defined a TF1 object (function) with name "funGEL"", function GausLin (above), 7 parameters
+        funGEL2->SetParameters((xmid+xmin)/2, (xmid-xmin)/2, ymax, 0,0,1,0,(xmax+xmid)/2, (xmax-xmid)/2,ymax); // We set initial parameters (just a guess)
+        funGEL2->SetParNames("Centroid1","Sigma1","PeakVal1", "BackgroundSlope","ErcHeight","ErcMult","Baseline","Centroid2","Sigma2","PeakVal2");   // We give the parameters a proper name (lineal part ax + b)
+        funGEL2->SetParLimits(0,xmin, xmid);            funGEL2->SetParLimits(7,xmid, xmax);
+        funGEL2->SetParLimits(1,0,(xmax-xmin));         funGEL2->SetParLimits(8,0,(xmax-xmin));
+        funGEL2->SetParLimits(2,0,1.5*ymax);            funGEL2->SetParLimits(9,0,1.5*ymax);
+        funGEL2->SetParLimits(4,0,1.5*ymax);
+
+        if(erc == '0') {funGEL2->FixParameter(4,0);}
+    hist->Fit("funGEL2","QR+"); //The R parameter restricts the fitting to [xmin, xmax]
+
+    return funGEL2;
+}
 
 void macro(string configFileName = "settings.example"){
-        ifstream configFile;
+    ifstream configFile;
     configFile.open(configFileName, ios::in);
     if(configFile.is_open()) cout<<"Reading configuration file.\n";
     string line;  while(getline(configFile,line)) if (line[0] == '>') break; // We skip until the first interesting line
@@ -70,9 +101,53 @@ void macro(string configFileName = "settings.example"){
     getline(configFile,line); const int N = stoi(line.substr(line.find("[")+1,line.find("]")-line.find("[")-1)); // we get N
 
     while(getline(configFile,line)) if (line[0] == '>') break; // We skip until the next interesting line
-    double_t x[N]; double_t Ex[N]; double_t mean[N]; double_t Emean[N]; double_t res[N]; double_t Eres[N];
+    double_t x[N]; double_t Ex[N]; double_t mean[N]; double_t Emean[N]; double_t sigma[N]; double_t Esigma[N];
+    TString histName; TString histTitle; string set; double_t xmin; double_t xmid; double_t xmax;
+    int i = 0;
+    while (true){
+        getline(configFile,line);
+        if (line[0] != '>') break; // If we finished reading those values, we exit.
+        histName = line.substr(line.find('"')+1,line.find('"',line.find('"')+1)-line.find('"')-1); line.erase(0,line.find('"',line.find('"')+1)+1);
+        histTitle = line.substr(line.find('"')+1,line.find('"',line.find('"')+1)-line.find('"')-1); line.erase(0,line.find('"',line.find('"')+1)+1);
+        
+        TH1D *hist = (TH1D*)fin->Get(histName);  hist->SetTitle(histTitle);  // We fetch the histogram from the file
+        std::cout<<"     Reading histogram "<<histTitle<<"\n";
+        while (true){
+            if(line.find("[") == -1) break;
+            set = line.substr(line.find("[")+1,line.find("]")-line.find("[")-1); line.erase(0,line.find("]")+1);
+            //std::cout<<"      >Performing fit on peak: "<<set<<"\n";
+            char opt = set[1];
+            if (set[0] == 'G'){
+                xmin = stod(set.substr(set.find(",")+1,set.find(",",set.find(",")+1))); set.erase(0,set.find(",",set.find(",")+1));
+                xmax = stod(set.substr(set.find(",")+1,set.find(",",set.find(",")+1)));
+                TF1 *funfit = fitGEL(hist, xmin, xmax);
+                    mean[i] = funfit->GetParameter(0); Emean[i] = funfit->GetParError(0);
+                    sigma[i] = funfit->GetParameter(1); Esigma[i] = funfit->GetParError(1);
+                    i++;
 
-    
+            } else if(set[1] == 'D'){
+                xmin = stod(set.substr(set.find(",")+1,set.find(",",set.find(",")+1))); set.erase(0,set.find(",",set.find(",")+1));
+                xmid = stod(set.substr(set.find(",")+1,set.find(",",set.find(",")+1))); set.erase(0,set.find(",",set.find(",")+1));
+                xmax = stod(set.substr(set.find(",")+1,set.find(",",set.find(",")+1))); set.erase(0,set.find(",",set.find(",")+1));
+                
+                TF1 *funfit = fitGEL2(hist, xmin, xmid, xmax);
+                    mean[i] = funfit->GetParameter(0); Emean[i] = funfit->GetParError(0);
+                    sigma[i] = funfit->GetParameter(1); Esigma[i] = funfit->GetParError(1);
+                    i++;
+
+                    mean[i] = funfit->GetParameter(7); Emean[i] = funfit->GetParError(7);
+                    sigma[i] = funfit->GetParameter(8); Esigma[i] = funfit->GetParError(8);
+                    i++;
+            }
+            
+        }
+        TCanvas *c1 = new TCanvas();
+        hist->Draw();
+    }
+
+
+
+
 
 
 }
